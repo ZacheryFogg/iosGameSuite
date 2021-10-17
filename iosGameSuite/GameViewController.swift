bb0bea8586985c8beb3d7f820de26d6816ef37b6
Zach Fogg <z.fogg1234@gmail.com>
2021-10-17 14:22:23 -0400

    Test commit

--- iosGameSuite/GameViewController.swift
+++ iosGameSuite/GameViewController.swift
@@ -10,7 +10,7 @@ import SpriteKit
 import GameplayKit
 
 class GameViewController: UIViewController {
-
+    // This is a comment
     override func viewDidLoad() {
         super.viewDidLoad()
         
